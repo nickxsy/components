2e2842d41ff6226c85022dab6f7f4590548e89c5
nickxsy <104327959+nickozy@users.noreply.github.com>
2024-10-09 00:34:17 +0300

    init, add Scroll

---
+++ Scroll/Scroll.js
@@ -0,0 +1,36 @@
+import { module } from "modujs";
+
+export default class Scroll extends module {
+  constructor(m) {
+    super(m);
+  }
+
+  init() {
+
+  }
+
+  onResize() {
+
+  }
+
+
+  bindEvents() {
+
+  }
+
+  unbindEvents() {
+
+  }
+
+  stop() {
+
+  }
+
+  start() {
+
+  }
+
+  destroy() {
+
+  }
+}
